5a2ba140c28e3d8640334a2b34a320196bf0c46d
Ricardo Morgan <162232723+KONGTHEBEAST@users.noreply.github.com>
2025-11-02 16:09:04 +0000

    Add files via upload

---
+++ Todolist folder/Form filler.py
@@ -0,0 +1,10 @@
+n = int(input("Enter a number"))
+n1 = int(input("Enter a second number"))
+n2 = input("Enter an operator + - * /")
+
+if n2 == "+":
+    sum = n1 + n2
+    print(f"the sum of these two numbers is {sum}")
+elif n2 == "-":
+    difference = n1 - n2 
+    print(f"The difference between these two numbers is {difference}")
